e3f3c86ff4b46f302adf72cda32ae9282374cfc9
Diego Nicoleti <diego.nicoleti@smokerstabacaria.com.br>
2023-09-29 23:06:49 -0300

    Começando Cadastro Psicólogo

--- help-me/src/core/psychologistRegistration/view.tsx
+++ help-me/src/core/psychologistRegistration/view.tsx
@@ -1,14 +1,14 @@
-import UserRegistration from './userRegistration.svg';
 
 export default function View(){
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex">
-        <div className="bg-primary-800 rounded-lg">
+        <div className="bg-gray-500 rounded-lg">
           <div className="ml-16 mt-8 mb-8 mr-14">
             <p className="font-semibold text-white text-xl mb-5">Crie sua conta</p>
             <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="Nome"/>
             <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="Sobrenome"/>
+            <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="CRP"/>
             <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="E-mail"/>
             <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="Senha"/>
             <input className="rounded-lg py-2 pl-5 mb-4 w-full block" type="text" placeholder="Confirme a senha"/>
@@ -16,14 +16,14 @@ export default function View(){
               Ao se registrar, você aceita nossos 
               <span className='text-gray-900'> termos de uso</span> e a nossa <span className='text-gray-900'>política de privacidade</span>.
             </p>
-            <button className='text-sm bg-gray-900 hover:bg-gray-800 text-white font-bold	py-2.5 px-4 rounded-lg w-full'>
+            <button className='text-sm bg-primary-800 hover:bg-primary-900 text-white font-bold	py-2.5 px-4 rounded-lg w-full'>
               CADASTRAR
             </button>
           </div>
         </div>
         
         <div>
-          <img src={UserRegistration} alt="" />
+          {/* <img src={UserRegistration} alt="" /> */}
         </div>
       </div>
     </div>
